8ed2adadd5691ba7d1da14e75aa1e6f8766d07b0
JingYu Ning <foldfelis@gmail.com>
2022-05-29 17:32:30 +0800

    Implement proj

--- src/MyPkgTemplate.jl
+++ src/MyPkgTemplate.jl
@@ -1,5 +1,26 @@
 module MyPkgTemplate
 
-# Write your package code here.
+using PkgTemplates
+
+export t
+
+const T = Template(
+    user="foldfelis",
+    dir=pwd(),
+    julia=v"1.6",
+    plugins=[
+        Codecov(),
+        GitHubActions(
+            linux=true,
+            windows=true,
+            x64=true,
+            coverage=true,
+            extra_versions=["1.6", "1.7", "nightly"],
+        ),
+        Documenter{GitHubActions}(),
+    ],
+)
+
+t(name::String) = T(name)
 
 end
